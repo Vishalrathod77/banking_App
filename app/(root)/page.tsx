d32a2bedcf5d432f24e468d4ce2002b04a01b215
Lab_db <vishalnenavath1337@gmail.com>
2024-12-10 18:27:58 -0500

    Update app with new features and components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,40 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalancebox from '@/components/TotalBalancebox';
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = { firstName: "Vishal", lastName: "Nenavath", email: "vishalnenavath1337@gmail.com" };
+
     return (
-        <div>Home</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Acess and manage your account and transactions efficiently"
+                    />
+
+                    <TotalBalancebox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={12500.35}
+
+
+                    />
+                </header>
+
+                RECENT TRANSACTIONS
+            </div>
+
+            <RightSidebar
+                user={loggedIn}
+                transactions={[]}
+                banks={[{ currentBalance: 123.50 }, { currentBalance: 2500.40 }]}
+            />
+        </section>
     )
 }
 
